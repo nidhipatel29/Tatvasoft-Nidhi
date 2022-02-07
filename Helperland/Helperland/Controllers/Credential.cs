af5de23ea96de5296e6f5c79dc48d539c940b1b1
nidhipatel29 <61034491+nidhipatel29@users.noreply.github.com>
2022-02-07 11:14:03 +0530

    SignUp and login Db integration

---
+++ Helperland/Helperland/Controllers/Credential.cs
@@ -0,0 +1,24 @@
+﻿using Helperland.Data;
+using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class Credential : Controller
+    {
+        private readonly HelperlandContext _helperlandcontext;
+        public Credential(HelperlandContext helperlandContext) 
+        {
+            _helperlandcontext = helperlandContext;
+        }
+        public IActionResult UserRegistration()
+        {
+            User user = new User();
+            return View(user);
+        }
+    }
+}
--- Helperland/Helperland/Controllers/HomeController.cs
+++ Helperland/Helperland/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Helperland.Models;
+﻿using Helperland.Data;
+using Helperland.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,6 +14,12 @@ namespace Helperland.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly HelperlandContext _helperlandcontext =new HelperlandContext();
+        //public HomeController(HelperlandContext helperlandContext)
+        //{
+        //    _helperlandcontext = helperlandContext;
+        //}
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,7 +27,8 @@ namespace Helperland.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            User user = new User();
+            return View(user);
         }
 
         public ViewResult Price()
@@ -28,6 +36,65 @@ namespace Helperland.Controllers
             return View();
         }
 
+        public ViewResult Faq()
+        {
+            return View();
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult UserRegistration()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+
+        public IActionResult UserRegistration(User user)
+        {
+            if (_helperlandcontext.Users.Where(x => x.Email == user.Email).Count() == 0 && _helperlandcontext.Users.Where(x => x.Mobile == user.Mobile).Count()==0)
+            {
+                user.UserTypeId = 1;
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                user.IsRegisteredUser = true;
+                user.ModifiedBy = 123;
+
+                _helperlandcontext.Users.Add(user);
+                _helperlandcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                ViewBag.message = "User already exists";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (_helperlandcontext.Users.Where(x => x.Email == user.Email).Count() == 0 && _helperlandcontext.Users.Where(x => x.Password == user.Password).Count() == 0)
+            {
+                ViewBag.Message = "Enter valid Email Id";
+            }
+            else
+            {
+                ViewBag.Message = "Login Successfully";
+                return RedirectToAction("ContactUs");
+            }
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
